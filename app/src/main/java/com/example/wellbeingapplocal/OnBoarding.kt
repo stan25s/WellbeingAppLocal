48911765266fe2cd8317d8675e83628a46848a81
stan25s <stansmith2501@gmail.com>
2022-03-21 12:37:01 +0000

    Added Working ViewPager2

--- app/src/main/java/com/example/wellbeingapplocal/OnBoarding.kt
+++ app/src/main/java/com/example/wellbeingapplocal/OnBoarding.kt
@@ -2,10 +2,50 @@ package com.example.wellbeingapplocal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.wellbeingapplocal.databinding.ActivityOnboardingBinding
 
 class OnBoarding : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViewPager2()
+    }
+
+    override fun onBackPressed() {
+        val viewPager = binding.viewPager
+        if (viewPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.currentItem = viewPager.currentItem - 1
+        }
+    }
+
+
+    private fun setupViewPager2() {
+        val list: MutableList<String> = ArrayList()
+        list.add("This is your First Screen")
+        list.add("This is your Second Screen")
+        list.add("This is your Third Screen")
+        list.add("This is your Fourth Screen")
+
+        val colorList: MutableList<String> = ArrayList()
+        colorList.add("#f0f0f0")
+        colorList.add("#f0f0f0")
+        colorList.add("#f0f0f0")
+        colorList.add("#f0f0f0")
+
+        // Set adapter to viewPager.
+        binding.viewPager.adapter = OnBoardingAdapter(this, list, colorList)
+
     }
 }
--- app/src/main/java/com/example/wellbeingapplocal/OnBoardingAdapter.kt
+++ app/src/main/java/com/example/wellbeingapplocal/OnBoardingAdapter.kt
@@ -1,20 +1,42 @@
 package com.example.wellbeingapplocal
 
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.wellbeingapplocal.ui.home.HomeFragment
+import com.example.wellbeingapplocal.databinding.Onboarding1Binding
 
-class OnBoardingAdapter(activity: AppCompatActivity, val itemsCount: Int) :
-    FragmentStateAdapter(activity) {
+
+class OnBoardingAdapter(private val context: Context,
+                        private val labelList: MutableList<String>,
+                        private val colorList: MutableList<String>) :
+    RecyclerView.Adapter<OnBoardingAdapter.ViewPagerHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerHolder {
+        val binding = Onboarding1Binding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewPagerHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
+        val label = labelList[position]
+        val color = colorList[position]
+        holder.bind(label, color)
+    }
 
     override fun getItemCount(): Int {
-        return itemsCount
+        return labelList.size
     }
 
-    override fun createFragment(position: Int): Fragment {
-        //return OnboardingFragment.getInstance(position)
-        return HomeFragment.getInstance(position)
+    class ViewPagerHolder(private var onboarding1Binding: Onboarding1Binding) :
+        RecyclerView.ViewHolder(onboarding1Binding.root) {
+        fun bind(label: String, color: String) {
+            onboarding1Binding.label.text = label
+            onboarding1Binding.root.setBackgroundColor(Color.parseColor(color))
+        }
     }
 }
-
