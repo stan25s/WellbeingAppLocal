c0fcb098a6032d97c0eb404feab3bd28297ab84c
Stanley Smith <stansmith2501@gmail.com>
2022-04-05 13:27:14 +0100

    Renamed notification page and related files

--- app/src/main/java/com/example/wellbeingapplocal/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/wellbeingapplocal/ui/profile/ProfileViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.wellbeingapplocal.ui.notifications
+package com.example.wellbeingapplocal.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
